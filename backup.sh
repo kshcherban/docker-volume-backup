b165fa6245b79ac07e18d64cd63d5bffdde0d9ae
kshcherban <k.scherban@gmail.com>
2019-01-24 23:45:23 +0100

    Use numeric owners for backup

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@ BACKUP_SRC=/backup
 BACKUP_DST=/opt/backup.tar.zst
 
 if [ "$1" == "backup" ]; then
-    tar -Izstd -cf $BACKUP_DST -C $BACKUP_SRC .
+    tar -Izstd --numeric-owner -cf $BACKUP_DST -C $BACKUP_SRC .
     chown $USER_ID $BACKUP_DST
 else
-    tar -Izstd -xf $BACKUP_DST -C $BACKUP_SRC
+    tar -Izstd --numeric-owner -xf $BACKUP_DST -C $BACKUP_SRC
 fi
